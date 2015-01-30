428f9ec7580b33c88ae0b3a2308c1e02363e42cd
Radu Helstern <radu.helstern@gmail.com>
2015-01-30 03:01:23 +0000

    more install script fixes

--- puppet/modules/ruby-install/files/install-postmodern.sh
+++ puppet/modules/ruby-install/files/install-postmodern.sh
@@ -13,6 +13,6 @@ TARGET_DIR="/tmp/ruby-install-${1}"
 SOURCE_ARCHIVE="${2}"
 
 mkdir ${TARGET_DIR}
-tar -xzvf "${SOURCE_ARCHIVE}" --directory ${TARGET_DIR}
+tar -xzvf "${SOURCE_ARCHIVE}" --directory ${TARGET_DIR} --strip-components=1
 cd ${TARGET_DIR} && make install
 cd /tmp && rm --force --recursive ${TARGET_DIR}
