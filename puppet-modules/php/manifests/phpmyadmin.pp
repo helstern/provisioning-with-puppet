7a1710c33ccc3788f6b52af7ce97f0934f88d722
radu helstern <radu.helstern@gmail.com>
2015-04-05 22:38:43 +0100

    change camelcase in preseed file class as it caused strange failures using versions  of puppet newer than 2.7

--- puppet-modules/php/manifests/phpmyadmin.pp
+++ puppet-modules/php/manifests/phpmyadmin.pp
@@ -5,7 +5,7 @@ class php::phpmyadmin
   $responseFile = 'phpmyadmin.seed'
 
   # copy the response file
-  bootstrap::preseedFile { $responseFile:
+  bootstrap::preseed_file { $responseFile:
     source => 'puppet:///modules/php/phpmyadmin.seed'
   }
 
@@ -14,7 +14,7 @@ class php::phpmyadmin
     ensure => present,
     responsefile => "$bootstrap::preseed::dir$responseFile",
     require => [
-      Bootstrap::PreseedFile[$responseFile],
+      Bootstrap::Preseed_file[$responseFile],
       Package['php5', 'php5-mysql', 'apache2']
     ],
   }
